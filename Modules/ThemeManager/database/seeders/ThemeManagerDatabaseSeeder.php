b6c710bb2d9056f16f0acdd7015d23159d1b3f02
morpheusadam <morpheusadam95@gmail.com>
2024-09-11 20:38:37 +0330

    add module theme manager

---
+++ Modules/ThemeManager/database/seeders/ThemeManagerDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\ThemeManager\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ThemeManagerDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/ThemeManager/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('thememanager::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('thememanager.name') !!}</p>
+@endsection
